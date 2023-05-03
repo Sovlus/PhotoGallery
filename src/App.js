92581202a0e2512f2d3ec238e686abfbc3a9ab18
Sovlus <kubbaz122@gmail.com>
2023-05-03 23:17:36 +0200

    added firebase config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React, { useState } from 'react';
 import firebase from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 import './App.css';
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDanxhovzCjDt8K0GdaAmngQsX_MsfCpEg",
+  authDomain: "gallery-64590.firebaseapp.com",
+  projectId: "gallery-64590",
+  storageBucket: "gallery-64590.appspot.com",
+  messagingSenderId: "45531961141",
+  appId: "1:45531961141:web:4c538f9c402ebb0d90de7c"
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
