f6f1f54dd45d28e5873772c0403293f0bd288551
Sovlus <kubbaz122@gmail.com>
2023-05-07 14:04:05 +0200

    fixed use stroage

--- src/Comps/useStorage.js
+++ src/Comps/useStorage.js
@@ -1,28 +1,30 @@
-import { useState, useEffect } from "react";
-import { projectStorage } from "../firebase/config";
+import { useState, useEffect } from 'react';
+import { projectStorage, projectFirestore, timestamp } from '../firebase/config';
 
 const useStorage = (file) => {
-    const [progress, setProgress] = useState(0);
-    const [error, setError] = useState(null);
-    const [url, setUrl] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    // tutaj upload
+    const storageRef = projectStorage.ref(file.name);
+    const collectionRef = projectFirestore.collection('images');
     
-    useEffect(() => {
-        // gdzie bedzie upload
-        const storageUpload = projectStorage.ref(file.name);
+    storageRef.put(file).on('state_changed', (snap) => {
+      let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+      setProgress(percentage);
+    }, (err) => {
+      setError(err);
+    }, async () => {
+      const url = await storageRef.getDownloadURL();
+      const createdAt = timestamp();
+      await collectionRef.add({ url, createdAt });
+      setUrl(url);
+    });
+  }, [file]);
 
-        storageUpload.put(file).on('state_changed', (snap) => {
-            let procent = (snap.bytesTransferred / snap.totalBytes) * 100
-            setProgress(procent);
-        }, (err) =>{
-            setError(err)
-        }, async() => {
-            const url = await storageUpload.getDownloadURL();
-            setUrl(url);
-        })
-    }, [file])
-
-    return { progress, url, error }
-
+  return { progress, url, error };
 }
 
 export default useStorage;
