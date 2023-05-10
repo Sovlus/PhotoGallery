dbf3ef8e998b0d936b3857f9aea7f803b925cbdb
Sovlus <kubbaz122@gmail.com>
2023-05-11 00:00:43 +0200

    fixed err

--- src/Comps/ImageGrid.js
+++ src/Comps/ImageGrid.js
@@ -2,16 +2,16 @@ import React from 'react';
 import useFirestore from './useFirestore';
 import { motion } from 'framer-motion';
 
-const ImageGrid = () => {
+const ImageGrid = ({ wybrane }) => {
     const { dokumenty } = useFirestore('images')
     
     return (
         <div className='siatka'>
             {dokumenty && dokumenty.map(doc => (
-                <motion.div className='siatka-wrap' key={dokumenty={.id}}
+                <motion.div className='siatka-wrap' key={dokumenty.id}
                 layout
                 whileHover={{ opacity: 1 }}s
-                onClick={() => setSelectedImg(dokumenty.url)}
+                onClick={() => wybrane(doc.url)}
                 >
                      <motion.img src={doc.url} alt="wrzucono plik"
             initial={{ opacity: 0 }}
