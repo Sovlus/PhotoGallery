21f2020fc15b4ac29211248d298252aeb42d8878
Sovlus <kubbaz122@gmail.com>
2023-05-11 22:54:21 +0200

    fixed error

--- src/Comps/Main.js
+++ src/Comps/Main.js
@@ -1,42 +1,12 @@
 import React from "react";
 
-const MainComponent = () => {
-    
+const Main = () => {
     return (
         <div className="Main">
-        <h1>Firebase Gallery</h1>
-        <h2>Twoje zdjęcia:</h2>
+            <h1>Firebase Gallery</h1>
+            <h2>Twoje zdjęcia:</h2>
         </div>
-    )
-}
+    );
+};
 
-export default MainComponent;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default Main;
+export { Main };
