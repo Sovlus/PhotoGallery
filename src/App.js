d0ba2143aaa44eec1d55f61e923ff99fcef8fe0d
Sovlus <kubbaz122@gmail.com>
2023-05-11 22:55:51 +0200

    fixed erorrs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import UploadForm from './Comps/UploadForm';
 import ImageGrid from  './Comps/ImageGrid';
-
+import Main from './Comps/Main'
 
 
 function App(
--- src/Comps/Main.js
+++ src/Comps/Main.js
@@ -9,4 +9,4 @@ const Main = () => {
     );
 };
 
-export { Main };
+export default Main;
